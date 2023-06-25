9e6b77ab86b994700d79d34afbddcbb691acbac1
Petrov <petrov85@inbox.ru>
2023-06-26 01:36:59 +0300

    Work with RingBuffer

--- include/RingBuffer.h
+++ include/RingBuffer.h
@@ -4,17 +4,22 @@
 #include <vector>
 #include <assert.h>
 
+#include <map>
 //#include <iostream>
 
 template <typename T>
 class RingBuffer
 {
+    using size_map_t = std::map<size_t, size_t>;
+
 public:
 
     ///Конструктор
     RingBuffer(size_t initBufferSize)
+    //: _currentPosition(_setCurrentPosition(initBufferSize))
     : _bufferSize(initBufferSize)
     , _current   (defaultCurrent())
+    //, _data      (std::vector<T>(_currentPosition.size(), static_cast<T>(0)))
     , _data      (std::vector<T>(_bufferSize, static_cast<T>(0)))
     { };
 
@@ -25,14 +30,19 @@ public:
     void setBuffer(size_t newBufferSize)
     {
         _bufferSize =  newBufferSize;
+        //_currentPosition = _setCurrentPosition(newBufferSize);
+
         _current    =  defaultCurrent();
+        //_data       =  std::vector<T>(_currentPosition.size(), static_cast<T>(0));
         _data       =  std::vector<T>(_bufferSize, static_cast<T>(0));
     };
 
     ///Размер буфера
-    size_t size()  const
+    size_t size()  const noexcept
     {
+
         return _bufferSize;
+        //return _currentPosition.size();
     };
 
     ///Добавление нового значения в начало буфера
@@ -61,11 +71,29 @@ public:
 
 private:
 
+    //size_map_t     _currentPosition;
+
     size_t         _bufferSize;
     size_t         _current;
     std::vector<T> _data;
 
-    size_t _setBufferIndex(size_t index) {
+
+
+    size_t _setBufferIndex(size_t index)
+    {
+     /*
+
+        size_t _size = _currentPosition.size();
+        assert((index < _size) && "Index exceeds buffer size");
+
+        index += _current;
+        if (index < _size) {
+            return index;
+        };
+
+        index -= _size;
+        return index;
+*/
 
         assert((index < _bufferSize) && "Index exceeds buffer size");
 
@@ -76,17 +104,48 @@ private:
 
         index -= _bufferSize;
         return index;
+
     };
+
+/*
+    size_map_t _setCurrentPosition(size_t newSize)
+    {
+        assert(newSize > 0 && "NewSize too small");
+
+        size_map_t tmpCurrentPosition;
+
+        tmpCurrentPosition[0] = newSize - 1;
+
+        for (size_t current = 1; current < newSize; ++current) {
+            tmpCurrentPosition[current] = current - 1;
+        };
+
+        return tmpCurrentPosition;
+    };
+
 
     void _moveCurrent()
     {
+        _current = _currentPosition[_current];
+    };
+
+*/
+
+
+    void _moveCurrent()
+    {
+
         if (_current > 0) {
             --_current;
             return;
         };
 
        _current = _bufferSize - 1;
+
+
     };
+
+
 
     static constexpr size_t defaultCurrent() {
         return 0;
